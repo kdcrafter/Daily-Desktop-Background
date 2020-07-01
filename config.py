24cb3b0a871efa07a76a6415d293ff862bc43a87
Kurtis Dressel <kurtisdoubled@hotmail.com>
2020-07-01 01:39:52 -0500

    added error checking, image resizing, and selection in get_image

---
+++ config.py
@@ -0,0 +1,8 @@
+WIDTH = 1920
+HEIGHT = 1080
+
+NUMBER_COLOR = 64 # the number of colors that will be in the color quantized image
+
+MAX_IMAGES = 19000 # the maximum images requested from the unsplash api
+
+COMPRESSION_QUALITY = 100 # value between 0 and 100
--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import requests
 from PIL import Image
 from io import BytesIO
+from random import randint
 
 from keys import *
-
-UNSPLASH_URL = 'https://api.unsplash.com'
+from config import *
 
 def get_image():
-    response = requests.get(UNSPLASH_URL + '/photos/random/?client_id=' + UNSPLASH_ACCESS_KEY)
+    image_number = randint(1, MAX_IMAGES)
 
-    if response.status_code == 401:
-        print('request is not authorized')
-    elif response.status_code == 404:
-        print('request error')
+    response = requests.get('https://api.unsplash.com/search/photos/' +
+        '?client_id=' + UNSPLASH_ACCESS_KEY + 
+        '&query=desktop-background' +
+        '&orientation=landscape' +
+        '&page=' + str(image_number) +
+        '&per_page=1')
 
-    image_url = response.json()['urls']['raw']
+    if response.status_code != 200:
+        print('Status Code:', response.status_code)
+        print(response.json()['errors'])
+        return
+    elif response.json() == {}:
+        print('Status Code:', 500)
+        print(['Internal Server Error'])
+        return
+
+    image_url = response.json()['results'][0]['urls']['raw']
+    image_url += ('&q=' + str(COMPRESSION_QUALITY) +
+        '&fm=jpg' +
+        '&w=' + str(WIDTH) +
+        '&h=' + str(HEIGHT) +
+        '&fit=min')
+
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
     image.save('temp.jpg')
