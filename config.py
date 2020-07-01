491f8bee17e12ce633bc0a2044a5b59ccf79c881
Kurtis Dressel <kurtisdoubled@hotmail.com>
2020-07-01 16:42:42 -0500

    added kmeans color quantization

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-WIDTH = 1920
-HEIGHT = 1080
-
-NUMBER_COLOR = 64 # the number of colors that will be in the color quantized image
-
 MAX_IMAGES = 19000 # the maximum images requested from the unsplash api
 
 COMPRESSION_QUALITY = 100 # value between 0 and 100
+
+WIDTH = 100 # desktop pixel width and height
+HEIGHT = 100
+
+NUMBER_COLORS = 2 # the number of colors that will be in the color quantized image
+
+IMAGE_SAMPLE_SIZE = 100000000 # number of pixels to be sampled for the color quantization algorithm
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 from PIL import Image
 from io import BytesIO
 from random import randint
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.cluster import KMeans
 
 from keys import *
 from config import *
 
 def get_image():
     image_number = randint(1, MAX_IMAGES)
-
     response = requests.get('https://api.unsplash.com/search/photos/' +
         '?client_id=' + UNSPLASH_ACCESS_KEY + 
         '&query=desktop-background' +
@@ -34,6 +36,29 @@ def get_image():
 
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
-    image.save('temp.jpg')
+    
+    return image
 
-get_image()
+def color_quantize_image(image):
+    # Fit on sample of image
+    # TODO: check if this works with black and white images
+    print('Fit on sample of image')
+    image = np.asarray(image, dtype=np.uint8)
+    image_sample = shuffle(image).reshape((WIDTH * HEIGHT, 3))[:IMAGE_SAMPLE_SIZE]
+    kmeans = KMeans(n_clusters=NUMBER_COLORS).fit(image_sample)
+    clusters = np.around(kmeans.cluster_centers_).astype(np.uint8)
+
+    # Apply cluster colors
+    print('Apply cluster colors')
+    image = np.apply_along_axis(lambda x: clusters[kmeans.predict([x])[0]], 2, image)
+    print(image)
+    image = Image.fromarray(image) # TIDO: ensure that image is actually made up of NUMBER_COLORS colors
+
+    return image
+
+if __name__ == '__main__':
+    #image = get_image()
+    #image.save('temp.jpg')
+    image = Image.open('temp.jpg')
+    image = color_quantize_image(image)
+    image.save('color_quantized_temp.jpg')
